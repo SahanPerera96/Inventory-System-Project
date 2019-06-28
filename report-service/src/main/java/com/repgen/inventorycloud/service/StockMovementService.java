b781feb167d97d15d7bb1cbc095971453ec45017
SahanPerera96 <dsahanperera@gmail.com>
2019-06-28 22:37:28 +0530

    pdf generation

--- report-service/src/main/java/com/repgen/inventorycloud/service/StockMovementService.java
+++ report-service/src/main/java/com/repgen/inventorycloud/service/StockMovementService.java
@@ -1,8 +1,13 @@
 package com.repgen.inventorycloud.service;
 
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
+
+import com.repgen.inventorycloud.modal.StockMovementDetails;
 
 public interface StockMovementService {
 
-	ResponseEntity<?> fetchdetails(Integer itemId,Integer uomId,Integer brandId);
+	ResponseEntity<StockMovementDetails> fetchdetails(Integer itemId,Integer uomId,Integer brandId);
+	
+	ResponseEntity<?> generateReport();
 }
